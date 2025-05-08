251a39af9351f2c16d68643612a56409fbb79781
Ethan Kwan <edkwan1@gmail.com>
2025-05-08 17:29:18 -0500

    added analytics to layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+	<Analytics /> {/* Add this line */}
       </body>
     </html>
   );
